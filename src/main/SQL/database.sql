c44f8b19ee9b9c4eba9617b91284eee208723653
Jordan-Conway <D00244954@student.dkit.ie>
2023-03-20 13:07:20 +0000

    Added SQL file

---
+++ src/main/SQL/database.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE IF NOT EXISTS oopCA6;
+
+DROP TABLE IF EXISTS gemstones;
+
+CREATE TABLE gemstones (
+	id INT NOT NULL AUTO_INCREMENT,
+	gemName VARCHAR(50),
+	carats DOUBLE,
+	colour VARCHAR(50),
+	clarity ENUM('FL', 'IF', 'VVS1', 'VVS2', 'VS1', 'VS2', 'SI1', 'SI2', 'i1', 'i2', 'i3'),
+	PRIMARY KEY (id)
+);
+
+INSERT INTO gemstones(gemName, carats, colour, clarity) VALUES
+("Amethyst", 2.55, "Siberian", 'VVS2'),
+("Sapphire", 0.34, "Blue", 'SI1'),
+("Rose Quartz", 0.65, "Rose", 'SI2');

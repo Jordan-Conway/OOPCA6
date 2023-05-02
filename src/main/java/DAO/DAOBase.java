a2033dd10d688b8b9171630228898a4bb62a60b0
Jordan-Conway <D00244954@student.dkit.ie>
2023-05-02 17:05:30 +0100

    DAO Base is now abstract

--- src/main/java/DAO/DAOBase.java
+++ src/main/java/DAO/DAOBase.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class DAOBase {
+public abstract class DAOBase {
     public Connection getConnection(){
         String driver = "com.mysql.cj.jdbc.Driver";
         String url = "jdbc:mysql://localhost:3306/oopCA6";
